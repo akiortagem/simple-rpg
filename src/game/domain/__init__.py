98f3b6ccae993e121ea6fa1c5a48a4455ff2d30e
akiortagem <akiortagem@gmail.com>
2025-12-25 21:33:03 +0700

    Add tilemap parsing helpers

--- src/game/domain/__init__.py
+++ src/game/domain/__init__.py
@@ -2,9 +2,11 @@
 
 from .scenes import RenderableTilemapLayer
 from .tilemap_layer import TilemapLayer, TilesetDescriptor
+from .tilemap_parser import parse_tilemap
 
 __all__ = [
     "RenderableTilemapLayer",
     "TilemapLayer",
     "TilesetDescriptor",
+    "parse_tilemap",
 ]
